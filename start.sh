2e56e902306ba5f27e54561a4fe1c9c3296a0388
Vitalii Madiakin <vmadiakin@Vitaliis-MacBook-Air.local>
2023-11-27 12:23:26 +0300

    add ssl to start script

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-uvicorn main:app --host 0.0.0.0 --port 8000 &
+uvicorn main:app --host 0.0.0.0 --port 8000 --ssl-keyfile /app/ssl/privkey.pem --ssl-certfile /app/ssl/fullchain.pem &
 python bot.py
