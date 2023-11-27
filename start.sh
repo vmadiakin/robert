dee94f16e96b8c1b4baf0691c2f90a19edc912f5
Vitalii Madiakin <vmadiakin@Vitaliis-MacBook-Air.local>
2023-11-27 19:42:27 +0300

    add tables

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 uvicorn main:app --host 0.0.0.0 --port 8000 --ssl-keyfile /app/ssl/privkey.pem --ssl-certfile /app/ssl/fullchain.pem &
-python bot.py
+python bot.py &
+python database.py
